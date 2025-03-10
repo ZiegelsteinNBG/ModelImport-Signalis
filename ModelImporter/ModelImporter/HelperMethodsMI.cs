cc571c3ff67fdaf45b6039563d24fe4509e58963
ZiegelsteinNBG <nhatkhangvo00@gmail.com>
2025-03-10 13:07:25 +0100

    ModelImporter Project

---
+++ ModelImporter/ModelImporter/HelperMethodsMI.cs
@@ -0,0 +1,251 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Model_Importer
+{
+    public class HelperMethodsMI
+    {
+
+        public static GameObject copyObjectDDOL(String originPath, String newName, bool dontDestroy)
+        {
+            GameObject originObject = GameObject.Find(originPath);
+            if (originObject == null)
+            {
+                MelonLogger.Error($"Method copyObjectDDOL failed at originPath: {originPath}");
+                return null;
+            }
+            GameObject copy = GameObject.Instantiate(originObject);
+
+            copy.name = newName;
+            if (dontDestroy) GameObject.DontDestroyOnLoad(copy);
+            return copy;
+        }
+
+        public static bool copyModelSMR(GameObject originObject, GameObject destinationObject)
+        {
+            if (originObject == null)
+            {
+                MelonLogger.Error($"copyModel failed with OriginObject: {originObject}");
+                return false;
+            }
+            if (destinationObject == null) return false;
+
+            SkinnedMeshRenderer meshRendererOrigin = originObject.GetComponent<SkinnedMeshRenderer>();
+            SkinnedMeshRenderer meshRendererDestination = destinationObject.GetComponent<SkinnedMeshRenderer>();
+
+            if (meshRendererOrigin == null)
+            {
+                MelonLogger.Error($"copyModel failed Mesh: {originObject}");
+                return false;
+            }
+            if (meshRendererDestination == null)
+            {
+                MelonLogger.Error($"copyModel failed Mesh: {destinationObject.name}");
+                return false;
+            }
+            meshRendererDestination.materials = meshRendererOrigin.materials;
+            meshRendererDestination.sharedMesh = meshRendererOrigin.sharedMesh;
+            return true;
+        }
+        public static void setParent(String parentPath, String childPath)
+        {
+            GameObject parentObject = GameObject.Find(parentPath);
+            if (parentObject != null)
+            {
+                GameObject childObject = GameObject.Find(childPath);
+                if (childObject != null)
+                {
+                    childObject.transform.SetParent(parentObject.transform);
+                }
+                else
+                {
+                    MelonLogger.Error($"setParent: Child GameObject '{childPath}' not found.");
+                }
+            }
+            else
+            {
+                MelonLogger.Error($"setParent: Parent GameObject '{parentPath}' not found.");
+            }
+        }
+
+        public static void setParent(String parentPath, GameObject child)
+        {
+            GameObject parentObject = GameObject.Find(parentPath);
+            if (parentObject != null)
+            {
+                if (child != null)
+                {
+                    child.transform.SetParent(parentObject.transform);
+                }
+                else
+                {
+                    MelonLogger.Error($"setParent: Child GameObject '{child}' not found.");
+                }
+            }
+            else
+            {
+                MelonLogger.Error($"setParent: Parent GameObject '{parentPath}' not found.");
+            }
+        }
+
+        public static void setParent(GameObject parentObject, GameObject child)
+        {
+            if (parentObject != null)
+            {
+                if (child != null)
+                {
+                    child.transform.SetParent(parentObject.transform);
+                }
+                else
+                {
+                    MelonLogger.Error($"setParent: Child GameObject '{child}' not found.");
+                }
+            }
+            else
+            {
+                MelonLogger.Error($"setParent: Parent GameObject '{parentObject}' not found.");
+            }
+        }
+
+        public static bool setChildActive(String parentPath, String childActivate, bool active)
+        {
+            // Find Parent
+            GameObject parentObject = GameObject.Find(parentPath);
+            if (parentObject != null)
+            {
+                // Find Child
+                Transform childTransform = parentObject.transform.Find(childActivate);
+                if (childTransform != null)
+                {
+                    // Set ChildObject Active
+                    GameObject child = childTransform.gameObject;
+                    child.SetActive(active);
+                    return true;
+                }
+                else
+                {
+                    MelonLogger.Error($"setChildActive: Child GameObject '{childActivate}' not found within '{parentObject.name}'.");
+                    return false;
+                }
+            }
+            else
+            {
+                MelonLogger.Error($"setChildActive: Parent GameObject '{parentPath}' not found.");
+                return false;
+            }
+        }
+
+        public static bool setChildActive(GameObject parent, String childActivate)
+        {
+
+            if (parent != null)
+            {
+                // Find Child
+                Transform childTransform = parent.transform.Find(childActivate);
+                if (childTransform != null)
+                {
+                    // Set ChildObject Active
+                    GameObject child = childTransform.gameObject;
+                    child.SetActive(true);
+                    return true;
+                }
+                else
+                {
+                    MelonLogger.Error($"setChildActive: Child GameObject '{childActivate}' not found within '{parent.name}'.");
+                    return false;
+                }
+            }
+            else
+            {
+                MelonLogger.Error($"setChildActive: Parent GameObject '{parent}' not found.");
+                return false;
+            }
+        }
+        public static void copyComponent(GameObject dest, String comp_or, String name, bool setNull)
+        {
+            GameObject comp_copy = copyObjectDDOL(comp_or, name, false);
+            setParent(dest, comp_copy);
+            if (setNull)
+            {
+                comp_copy.transform.localPosition = Vector3.zero;
+                comp_copy.transform.localRotation = new Quaternion(0, 0, 0, 0);
+            }
+            else
+            {
+                comp_copy.transform.localPosition = GameObject.Find(comp_or).transform.localPosition;
+                comp_copy.transform.localRotation = GameObject.Find(comp_or).transform.localRotation;
+            }
+        }
+
+        public static Dictionary<String, int> dictList(SkinnedMeshRenderer skin)
+        {
+            if (skin == null)
+            {
+                MelonLogger.Error("boneList: Invalid param->null");
+                return null;
+            }
+            Dictionary<String, int> keyValuePairs = new Dictionary<String, int>();
+            int idx = 0;
+            foreach (Transform bone in skin.bones)
+            {
+                keyValuePairs.Add(bone.name, idx++);
+            }
+            return keyValuePairs;
+        }
+
+        public static void updatePose(SkinnedMeshRenderer origin, SkinnedMeshRenderer dest, Dictionary<String, int> dict)
+        {
+            try
+            {
+                foreach (Transform bone in origin.bones)
+                {
+                    Transform dest_Transform = dest.bones[dict[bone.name]];
+                     if ((bone.name == "shoulder_R" || bone.name == "shoulder_L") && (dest.name.Contains("Placeholder")) && PlayerState.aiming && InventoryManager.EquippedWeapon.name != "Shotgun") // Placeholder
+                    {
+                        Vector3 eul = bone.localEulerAngles;
+                        eul.y = eul.y * 1.065f;
+                        eul.z = eul.z * 0.99f;
+                        dest_Transform.localEulerAngles = eul;
+                        continue;
+                    }
+                    dest_Transform.localRotation = bone.localRotation;
+                    if (bone.name == "hips") dest_Transform.localPosition = bone.localPosition;
+                }
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error($"updatePost: failed updating Bones \nException: {e.Message}");
+            }
+        }
+
+        public static SkinnedMeshRenderer insertAlternative(GameObject model_or, String model_s, String name, String body, String normal, Dictionary<String, int> dict)
+        {
+            model_or.SetActive(true);
+            GameObject model = copyObjectDDOL(model_s, model_s, false);
+            model_or.SetActive(false);
+            setParent("__Prerequisites__/Character Origin/Character Root/Ellie_Default", model);
+
+            GameObject root = GameObject.Find($"__Prerequisites__/Character Origin/Character Root/Ellie_Default/{model_s}/{normal}/Root/");
+            root.name = $"Root_{name}";
+            setParent("__Prerequisites__/Character Origin/Character Root/Ellie_Default/metarig/", root);
+            root.transform.localPosition = Vector3.zero;
+            root.transform.localRotation = new Quaternion(0, 0, 0.7071f, 0.7071f);
+
+            SkinnedMeshRenderer skin = model.transform.Find(body).GetComponent<SkinnedMeshRenderer>();
+            skin.bones[dict["hips"]].localPosition = Vector3.zero;
+
+            copyComponent(skin.bones[dict["hand_R"]].gameObject, "__Prerequisites__/Character Origin/Character Root/Ellie_Default/metarig/Root/hips/spine/chest/shoulder_R/upper_arm_R/forearm_R/hand_R/WeaponMount/", "WeaponMount", true);
+            copyComponent(skin.bones[dict["hips"]].gameObject, "__Prerequisites__/Character Origin/Character Root/Ellie_Default/metarig/Root/hips/VisibleEquip/", "VisibleEquip", true);
+            copyComponent(skin.bones[dict["chest"]].gameObject, "__Prerequisites__/Character Origin/Character Root/Ellie_Default/metarig/Root/hips/spine/chest/Nitro Model/", "Nitro Model", false);
+            copyComponent(skin.bones[dict["chest"]].gameObject, "__Prerequisites__/Character Origin/Character Root/Ellie_Default/metarig/Root/hips/spine/chest/FlashLightFlare/", "FlashLightFlare", true);
+
+            return skin;
+        }
+    }
+}
---
+++ ModelImporter/ModelImporter/ModelImporter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using Il2CppSystem.IO;
+using UnityEngine.SceneManagement;
+using Il2Cpp;
+using Il2CppSystem;
+using UnityEngine.UI;
+
+
+namespace Model_Importer
+{
+    public class ModelImporter: MelonMod
+    {
+        private string dataPath;
+        string currentDirectory;
+        List<string> assetBundlePaths;
+        Il2CppAssetBundle ab;
+        GameObject player;
+        Dictionary<string, int> boneDict;
+        bool isLoaded = false;
+        public GameObject uiPanel;
+        public Slider heightSlider = new Slider();
+        public Slider playerSizeSlider = new Slider();
+        public Slider weaponSizeSlider = new Slider();
+        public Toggle dynamicHolsterToggle = new Toggle();
+        public Toggle[] weaponToggles = { new Toggle(), new Toggle() };
+        UnityEngine.Events.UnityAction<float> sa;
+        UnityEngine.Events.UnityAction<bool> ba;
+        public override void OnUpdate()
+        {
+            if(!isLoaded && SceneManager.GetActiveScene().name == "MainMenu")
+            {
+                LoadModel();
+                isLoaded = true;
+            }
+
+            //// Now add the listeners correctly
+            //heightSlider.onValueChanged.AddListener(sa);
+            //playerSizeSlider.onValueChanged.AddListener(sa);
+            //weaponSizeSlider.onValueChanged.AddListener(sa);
+            //dynamicHolsterToggle.onValueChanged.AddListener(ba);
+
+            //for (int i = 0; i < weaponToggles.Length; i++)
+            //{
+            //    weaponToggles[i].onValueChanged.AddListener(ba);
+            //}
+        }
+
+        private void LoadModel()
+        {
+            currentDirectory = System.Environment.CurrentDirectory;
+            dataPath = Path.Combine(currentDirectory, "Mods", "ModelImporter_Data");
+            MelonLogger.Msg($"Data Path: {dataPath} {Directory.Exists(dataPath)}");
+
+            foreach (string file in Directory.GetFiles(dataPath))
+            {
+                string fileNameNoEx = Path.GetFileNameWithoutExtension(file);
+                string fileName = Path.GetFileName(file);
+
+                if (fileName == fileNameNoEx)
+                {
+                    MelonLogger.Msg($"File: {fileName} | Without Extension: {fileNameNoEx} | File: {file}");
+                    if (!File.Exists(file))
+                    {
+                        MelonLogger.Error($"AssetBundle not found: {file}");
+                        continue;
+                    }
+                    try
+                    {
+                        byte[] fileData = File.ReadAllBytes(file);
+                        ab = Il2CppAssetBundleManager.LoadFromFile(file);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        MelonLogger.Error($"Exception while loading AssetBundle: {ex.Message}{ex.StackTrace}");
+                    }
+                    if (ab == null)
+                    {
+                        MelonLogger.Error($"Failed to load AssetBundle: {file}");
+                        continue;
+                    }
+
+                    player = GameObject.Instantiate(ab.LoadAsset<GameObject>("Ellie_Default").TryCast<GameObject>());
+                    if (player == null)
+                    {
+                        MelonLogger.Error($"Failed to load asset 'Model' from AssetBundle: {file}");
+                        continue;
+                    }
+                    foreach (Il2CppSystem.Object child in player.transform)
+                    {
+                        MelonLogger.Msg($"Child: {child.TryCast<Transform>().name}");
+                    }
+                    player.name = "Model";
+                    GameObject.DontDestroyOnLoad(player);
+                    ab.Unload(false);
+                    break;
+                }
+            }
+        }
+
+        // WIP
+        public void ApplyChanges()
+        {
+            Debug.Log("Applying changes...");
+        }
+
+
+        public void ResetToDefault()
+        {
+            Debug.Log("Reset to default settings.");
+        }
+
+        public void ToggleUI()
+        {
+            uiPanel.SetActive(!uiPanel.activeSelf);
+        }
+    }
+}
