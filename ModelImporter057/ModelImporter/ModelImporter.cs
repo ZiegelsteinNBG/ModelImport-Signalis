60a301b1db0feb3b4fd8d6d96be29de0dc63fee9
Ziegelstein <91370891+ZiegelsteinNBG@users.noreply.github.com>
2025-07-30 01:56:54 +0200

    Update ModelImporter.cs

--- ModelImporter057/ModelImporter/ModelImporter.cs
+++ ModelImporter057/ModelImporter/ModelImporter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ using UnityEngine.UI;
 using MeshCombineStudio;
 using UnhollowerBaseLib;
 using UnhollowerRuntimeLib;
+using static UnityEngine.GUI;
 
 
 namespace Model_Importer
@@ -24,23 +25,26 @@ namespace Model_Importer
         static SkinnedMeshRenderer body_skin;
         GameObject placeholder;
         GameObject player;
+        
 
-        // Camera
-        List<Camera> camerasOrtho = new List<Camera>();
-        bool orth = false;
         
         Dictionary<string, int> boneDict;
+        string modelName = "";
+        Dictionary<string, bool> bodyPartS;
+        Dictionary<string, Transform> bodyPart;
 
         // Cutscenehandler
         List<(GameObject, SkinnedMeshRenderer)> cutScenemodels;
         // CutSceneObject; CutSceneModel, boneDict
         List<(GameObject, SkinnedMeshRenderer, SkinnedMeshRenderer)> cutScenemodelsIdx;
+        Dictionary<int, Dictionary<string, Transform>> cutSceneTransformDict;
 
         // GUI
         private bool showGUI = false;
-        bool start = false;
+        bool start = false, passBodyPartS = false, newNames = true;
         private enum GUIMode { Main, Settings }
         private GUIMode currentMode = GUIMode.Main;
+        ModelImporterGUI modelImporterGUI;
 
         void startGui()
         {
@@ -50,6 +54,7 @@ namespace Model_Importer
             UnityEngine.Object.DontDestroyOnLoad(guiObject);
             guiObject.AddComponent<ModelImporterGUI>();
             guiObject.GetComponent<ModelImporterGUI>().enabled = true;
+            modelImporterGUI = guiObject.GetComponent<ModelImporterGUI>();
         }
 
         public override void OnUpdate()
@@ -79,15 +84,35 @@ namespace Model_Importer
 
                     foreach ((GameObject, SkinnedMeshRenderer, SkinnedMeshRenderer) model in cutScenemodelsIdx)
                     {
+
+                        GameObject parent = model.Item3.gameObject.transform.parent.gameObject;
                         if (model.Item1.activeInHierarchy)
                         {
                             HelperMethodsMI.updatePose(model.Item2, model.Item3, boneDict);
-                            model.Item3.gameObject.transform.parent.gameObject.SetActive(true);
+                            parent.SetActive(true);
+                            if(start)updateActive(cutSceneTransformDict[parent.GetHashCode()]);
                         }
                         else
                         {
-                            model.Item3.gameObject.transform.parent.gameObject.SetActive(false);
+                            parent.SetActive(false);
                         }
+                    }
+
+                    Transform edgecase = GameObject.Find("Cutscenes/Hatch Failure - Beyond/LAB_Hatch_1 Hatch Grab/Hatch_1/CharSpace/Ellie_Default")?.transform;
+                    if(edgecase != null )
+                    {
+                        edgecase.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                        edgecase.localPosition = new Vector3(1.3796f, 2.2134f, - 2.3104f);
+                    }
+                    if (start)
+                    {
+                        if (modelName != "" && !passBodyPartS)
+                        {
+                            modelImporterGUI.bodyParts = bodyPartS;
+                            passBodyPartS = true;
+                        }
+                        
+                        updateActive(bodyPart);
                     }
                 }
 
@@ -98,6 +123,7 @@ namespace Model_Importer
                         startGui();
                         start = true;
                     }
+
                     ModelImporterGUI.Instance?.ToggleGUI();
 
                 }
@@ -109,11 +135,8 @@ namespace Model_Importer
                 MelonLogger.Error($"Stack Trace: {ex.StackTrace}");
             }
         }
-        Il2CppReferenceArray<GUILayoutOption> option = (Il2CppReferenceArray<GUILayoutOption>)new GUILayoutOption[] { GUILayout.Width(200f) };
 
-
-
-        private void LoadModel()
+        void LoadModel()
         {
             // TODO Support multiple AB
             if(ab != null) ab.Unload(true);
@@ -163,7 +186,7 @@ namespace Model_Importer
                     {
                         MelonLogger.Msg($"Child: {child.TryCast<Transform>().name}");
                     }
-                    player.name = "Model";
+                    player.name = fileName;
                     
                     break;
                 }
@@ -183,11 +206,28 @@ namespace Model_Importer
                 }
                 if(body_skin.rootBone.name != "hips") body_skin.rootBone = player.transform.Find("metarig/Root/hips");
                 boneDict = HelperMethodsMI.dictList(body_skin);
-                // TODO could be better
                 placeholder = GameObject.Instantiate(player);
                 placeholder.SetActive(false);
                 body_skin = HelperMethodsMI.insertAlternative(player, "imp_Mo", "Body", "metarig", boneDict);
                 player.transform.localPosition = Vector3.zero;
+
+                
+                
+                
+                if (modelName != player.name)
+                {
+                    bodyPartS = new Dictionary<string, bool>();
+                    passBodyPartS = false;
+                }
+                bodyPart = new Dictionary<string, Transform>();
+                for (int i = 0; i < player.transform.childCount; i++)
+                {
+                    if(player.transform.GetChild(i).name == "metarig") continue;
+                    if (modelName != player.name) bodyPartS.Add(player.transform.GetChild(i).name, true);
+                    bodyPart.Add(player.transform.GetChild(i).name, player.transform.GetChild(i));
+                }
+                modelName = player.name;
+
             }
             else
             {
@@ -197,10 +237,33 @@ namespace Model_Importer
             }
         }
 
-        public void updatePose()
+        void updatePose()
         {
             SkinnedMeshRenderer defaultSkin = GameObject.Find("__Prerequisites__/Character Origin/Character Root/Ellie_Default/Normal/Body").GetComponent<SkinnedMeshRenderer>();
-            HelperMethodsMI.updatePose(defaultSkin, body_skin, boneDict);
+            if (!start) {
+                HelperMethodsMI.updatePose(defaultSkin, body_skin, boneDict);
+            }
+            else
+            {
+                HelperMethodsMI.updatePose(defaultSkin, body_skin, boneDict, modelImporterGUI.sliderValueY, modelImporterGUI.sliderValueZ);
+            }
+            
+        }
+
+        void updateActive(Dictionary<string, Transform> bodyPartTransform)
+        {
+            if (passBodyPartS)
+            {
+                MelonLogger.Msg($"Updating body parts for {modelName} {modelImporterGUI.bodyParts}");
+                foreach (KeyValuePair<string, bool> kvp in modelImporterGUI.bodyParts)
+                {
+                    if (bodyPartTransform.ContainsKey(kvp.Key))
+                    {
+                        MelonLogger.Msg($"Setting {kvp.Key} to {kvp.Value}");
+                        bodyPartTransform[kvp.Key].gameObject.SetActive(kvp.Value);
+                    }
+                }
+            }
         }
 
         void insertModelCutScenes()
@@ -217,6 +280,7 @@ namespace Model_Importer
                 deactivateDefaultModels(GameObject.Find("Cutscenes/Hatch Failure - Beyond/LAB_Hatch_2 Silhouette Pull/Wide_1/Elster/"), ref cutScenemodels, true);
             }
             int idx = 0;
+            cutSceneTransformDict = new Dictionary<int, Dictionary<string, Transform>>();
             foreach ((GameObject, SkinnedMeshRenderer) model in cutScenemodels)
             {
                 GameObject cutSceneModel = GameObject.Instantiate(placeholder);
@@ -236,12 +300,21 @@ namespace Model_Importer
                 cutSceneModel.SetActive(false);
                 cutScenemodelsIdx.Add((model.Item1, model.Item2, cutSceneModel.transform.Find("Body").GetComponent<SkinnedMeshRenderer>()));
                 cutSceneModel.transform.localPosition = Vector3.zero;
+
+                MelonLogger.Msg($"Cutscenemode: {cutSceneModel.name} {cutSceneModel.GetHashCode()}");
+                Dictionary<string, Transform> modelPartDict = new Dictionary<string, Transform>();
+                for (int i = 0; i < cutSceneModel.transform.childCount; i++)
+                {
+                    if (cutSceneModel.transform.GetChild(i).name == "metarig") continue;
+                    modelPartDict.Add(cutSceneModel.transform.GetChild(i).name, cutSceneModel.transform.GetChild(i));
+                }
+                cutSceneTransformDict.Add(cutSceneModel.GetHashCode(), modelPartDict);
             }
 
         }
 
         // Make all DefaultModels invisble
-        public void deactivateDefaultModels(GameObject parent, ref List<(GameObject, SkinnedMeshRenderer)> models, bool ign)
+        void deactivateDefaultModels(GameObject parent, ref List<(GameObject, SkinnedMeshRenderer)> models, bool ign)
         {
             try
             {
@@ -297,9 +370,6 @@ namespace Model_Importer
                 MelonLogger.Error($"Stack Trace: {ex.StackTrace}");
             }
         }
-
-
-        
     }
     [RegisterTypeInIl2Cpp]
     public class ModelImporterGUI : MonoBehaviour
@@ -310,16 +380,20 @@ namespace Model_Importer
         public enum GUIMode { Main, Settings }
         public GUIMode currentMode = GUIMode.Main;
 
-        private float sliderValue = 1.0f;
-        private float maxSliderValue = 2.0f;
+        public float sliderValueY = 1.0f;
+        public float sliderValueZ = 1.0f;
+        public Dictionary<string, bool> bodyParts;
+        private Vector2 scrollViewVector = Vector2.zero;
 
         private Il2CppReferenceArray<GUILayoutOption> option =
             (Il2CppReferenceArray<GUILayoutOption>)new GUILayoutOption[] { GUILayout.Width(200f) };
+        private Rect windowRect = new Rect(0, 0, 500, 600);
 
         public ModelImporterGUI(IntPtr ptr) : base(ptr) { }
         public void ToggleGUI()
         {
             showGUI = !showGUI;
+            if(showGUI) currentMode = GUIMode.Main;
             MelonLogger.Msg($"GUI is now {(showGUI ? "visible" : "hidden")}");
         }
 
@@ -330,47 +404,101 @@ namespace Model_Importer
 
         void OnGUI()
         {
-            GUI.Label(new Rect(10, 10, 200, 20), "DEBUG: GUI is drawing!");
+            //GUI.Label(new Rect(10, 10, 200, 20), "DEBUG: GUI is drawing!");
+            if (textAreaStringZ == "null") textAreaStringZ = $"{sliderValueZ}";
+            if (textAreaStringY == "null") textAreaStringY = $"{sliderValueY}";
+            windowRect = GUILayout.Window(0, windowRect, (WindowFunction)WindowFunction, "Model Importer", option);
+        }
+
+        void WindowFunction(int windowID)
+        {
             if (!showGUI) return;
 
-            GUILayout.BeginArea(new Rect(0, 0, 300, 200));
+            //GUILayout.BeginArea(new Rect(0, 0, 900, 600));
             GUILayout.BeginHorizontal(option);
-            if (GUILayout.Button("Main", option)) currentMode = GUIMode.Main;
-            if (GUILayout.Button("Settings", option)) currentMode = GUIMode.Settings;
+            if (GUILayout.Button("Arm adjustment", option)) currentMode = GUIMode.Main;
+            if (GUILayout.Button("Body parts", option)) currentMode = GUIMode.Settings;
             GUILayout.EndHorizontal();
             GUILayout.Space(10);
 
             switch (currentMode)
             {
                 case GUIMode.Main:
+                    GUILayout.BeginHorizontal(option);
+                    GUILayout.Label("Adjust the relativ arm distance to fix overlapping arms when aiming\nY is horizontal, Z vertical", option);
+                    GUILayout.Label("----------------------------------------------", option);
+
+                    GUILayout.Space(10);
+                    GUILayout.EndHorizontal();
                     DrawMainTab();
                     break;
                 case GUIMode.Settings:
+                    if (bodyParts == null || bodyParts.Count == 0)
+                    {
+                        GUILayout.Label("No body parts found. Please load a model first.", option);
+                        return;
+                    }
                     DrawSettingsTab();
                     break;
             }
 
-            GUILayout.EndArea();
+            //GUILayout.EndArea();
         }
-
+        string textAreaStringZ = "null";
+        string textAreaStringY = "null";
         void DrawMainTab()
         {
             GUILayout.BeginHorizontal(option);
-            if (GUILayout.RepeatButton("Increase max\nSlider Value", option))
+            if (GUILayout.Button("Reset left arm y-axis", option))
             {
-                maxSliderValue += 3.0f * Time.deltaTime;
+                sliderValueY = 1.0f;
+                textAreaStringY = $"{sliderValueY}";
             }
 
             GUILayout.BeginVertical(option);
-            GUILayout.Box("Slider Value: " + Mathf.Round(sliderValue), option);
-            sliderValue = GUILayout.HorizontalSlider(sliderValue, -2.0f, maxSliderValue, option);
+
+            GUILayout.Label("Left arm y-axis distance", option);
+            textAreaStringY = GUILayout.TextArea(textAreaStringY, option);
+            if (float.TryParse(textAreaStringY, out float result))
+            {
+                sliderValueY = result;
+            }
+            GUILayout.EndVertical();
+
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal(option);
+
+            if (GUILayout.Button("Reset left arm z-axis", option))
+            {
+                sliderValueZ = 1.0f;
+                textAreaStringZ = $"{sliderValueZ}";
+            }
+
+            GUILayout.BeginVertical(option);
+            GUILayout.Label("Left arm z-axis distance", option);
+            textAreaStringZ = GUILayout.TextArea(textAreaStringZ, option);
+            if (float.TryParse(textAreaStringZ, out float result1))
+            {
+                sliderValueZ = result1;
+            }
+            
             GUILayout.EndVertical();
             GUILayout.EndHorizontal();
         }
 
         void DrawSettingsTab()
         {
-            GUILayout.Label("Settings go here...", option);
+            scrollViewVector = GUILayout.BeginScrollView(scrollViewVector, option);
+            for (int i = 0; i < bodyParts.Count; i++)
+            {
+                string key = bodyParts.Keys.ElementAt(i);
+                GUILayout.Label(key, option);
+                if (GUILayout.Button(bodyParts[key] ? $"Hide {bodyParts[key]}" : $"Show {bodyParts[key]}", option))
+                {
+                    bodyParts[key] = !bodyParts[key];
+                }
+            }
+            GUILayout.EndScrollView();
         }
     }
     /// <summary>
